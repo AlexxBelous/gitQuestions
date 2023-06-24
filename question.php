4f9b2da7fad32a4ab553d585f00f86aca179c18d
AlexxBelous <connectionwithme1@gmail.com>
2023-06-24 11:45:03 +0300

    fix

--- question.php
+++ question.php
@@ -43,25 +43,25 @@
 
 <?php
 
-// function duplicates($arr)
-// {
-//     $dub = [];
-//     foreach ($arr as $key => $value) {
-//         if ($key != array_search($value, $arr, TRUE) and !in_array($value, $dub)) $dub[] = $value;
-//     }
-//     return $dub;
-// }
+function duplicates($arr)
+{
+    $dub = [];
+    foreach ($arr as $key => $value) {
+        if ($key != array_search($value, $arr, TRUE) and !in_array($value, $dub)) $dub[] = $value;
+    }
+    return $dub;
+}
 
 
 
-// $array1 = [1, 2, 3, 2, 4, 4, 5];
-// $result1 = duplicates($array1);
-// print_r($result1);
+$array1 = [1, 2, 3, 2, 4, 4, 5];
+$result1 = duplicates($array1);
+print_r($result1);
 
 
-// $array2 = ['apple', 'banana', 'cherry', 'banana', 'durian'];
-// $result2 = duplicates($array2);
-// print_r($result2);
+$array2 = ['apple', 'banana', 'cherry', 'banana', 'durian'];
+$result2 = duplicates($array2);
+print_r($result2);
 
 
 
