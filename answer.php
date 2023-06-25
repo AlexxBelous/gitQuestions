79f8652238280ebf3a0ba98e880915a6eeef29d2
AlexxBelous <connectionwithme1@gmail.com>
2023-06-25 13:38:58 +0300

    que-3

--- answer.php
+++ answer.php
@@ -3,18 +3,6 @@
 // Код ниже 
 
 
+$fruits = ['apple', 'banana', 'orange', 'grape'];
 
-
-
-
-
-
-
-
-
-
-
-$array1 = [1, 2, 3, 2, 4, 4, 5];
-
-$array2 = ['apple', 'banana', 'cherry', 'banana', 'durian'];
 
--- question.php
+++ question.php
@@ -1,4 +1,4 @@
-<!-- Напиши function duplicates, которая принимает один аргумент $arr, представляющий собой массив, и возвращает массив, содержащий все повторяющиеся элементы из исходного массива. -->
+<!-- Как с использованием функции array_search() можно найти индекс элемента "orange" в массиве $fruits и вывести его значение? -->
 
 
 
@@ -43,25 +43,16 @@
 
 <?php
 
-function duplicates($arr)
-{
-    $dub = [];
-    foreach ($arr as $key => $value) {
-        if ($key != array_search($value, $arr, TRUE) and !in_array($value, $dub)) $dub[] = $value;
-    }
-    return $dub;
+
+$fruits = ['apple', 'banana', 'orange', 'grape'];
+
+$index = array_search('apple', $fruits);
+
+if ($index !== false) {
+    echo "Index of the found element: $index";
+} else {
+    echo "Item not found";
 }
-
-
-
-$array1 = [1, 2, 3, 2, 4, 4, 5];
-$result1 = duplicates($array1);
-print_r($result1);
-
-
-$array2 = ['apple', 'banana', 'cherry', 'banana', 'durian'];
-$result2 = duplicates($array2);
-print_r($result2);
 
 
 
