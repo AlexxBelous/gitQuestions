1ea13937c7ae5c7bc124835e88a864058f096a22
AlexxBelous <connectionwithme1@gmail.com>
2023-07-03 07:45:19 +0300

    fix

--- answer.php
+++ answer.php
@@ -3,16 +3,10 @@
 // Код ниже 
 
 
-function getMiddle($text) {
-	$start = floor((strlen($text) - 1) / 2);
-  	$len = strlen($text) % 2 ? 1 : 2;
-	return substr($text, $start, $len);
-}
 
 
 
 
 $string = "Lorem ipsum dolor sit amet";
-$result3 = getMiddle($string);
-echo $result3; //Output - "do"
+
 
