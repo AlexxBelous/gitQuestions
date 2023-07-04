3dcc0a21fe861bf533e8af6c0b6903bece4c6368
AlexxBelous <connectionwithme1@gmail.com>
2023-07-04 17:50:29 +0300

    fix

--- answer.php
+++ answer.php
@@ -3,24 +3,6 @@
 // Код ниже 
 
 
-
-
 // Original array
 $array = [2, 4, 6, 2, 4, 8, 4, 2];
 
-// Initialize an empty array to count repetitions
-$counts = [];
-
-// Count repetitions
-foreach ($array as $element) {
-    if (isset($counts[$element])) {
-        $counts[$element]++;
-    } else {
-        $counts[$element] = 1;
-    }
-}
-
-// Output the results
-foreach ($counts as $element => $count) {
-    echo "Element $element repeats $count time(s).<br>";
-}
