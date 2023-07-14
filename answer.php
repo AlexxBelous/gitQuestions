8886e8dba805fc5696faf20017eeeb641d41376b
AlexxBelous <connectionwithme1@gmail.com>
2023-07-14 07:22:14 +0300

    question 18

--- answer.php
+++ answer.php
@@ -2,11 +2,41 @@
 
 // Код ниже 
 // Максимально 5 / 3
+require 'country.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search Capital Country</title>
+</head>
+<body>
+
+<!-- форма ниже -->
 
 
 
 
-$string = 'This is the string in which you need to find the letter F';
+<?php 
+// Код php ниже
+
+
+
+
+?>
+
+
+    
+</body>
+</html>
+
+
+
+
+
+
 
 
 
---
+++ country.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+$countries = array(
+    "USA" => "Washington, D.C.",
+    "Russia" => "Moscow",
+    "China" => "Beijing",
+    "France" => "Paris",
+    "Germany" => "Berlin",
+    "Brazil" => "Brasília",
+    "Australia" => "Canberra"
+);
