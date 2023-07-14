076f7dc1fef0af9c9b8b480801164481c0868430
AlexxBelous <connectionwithme1@gmail.com>
2023-07-14 06:50:51 +0300

    question 17

--- answer.php
+++ answer.php
@@ -1,11 +1,27 @@
 <?php
-
 // Код ниже 
 // максимальная - 5 / 1
+?>
 
 
 
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Day of the Week</title>
+</head>
+
+<body>
+    <h1>Day of the Week</h1>
+    <!-- Форма ниже -->
 
 
-$string = "Hello, World!";
+    <?php
+    // php код ниже
 
+
+    ?>
+</body>
+
+</html>
--- question.php
+++ question.php
@@ -1,6 +1,4 @@
-<!-- Есть исходная строка, из которой нужно получить подстроку World с помощью substr().
-Для этого необходимо использовать три разных аргумент.
-
+<!-- Напишите программу на PHP где пользователь может ввести число от 1 до 7. После отправки формы, программа должна обработать введенное число и вывести соответствующий день недели на экран. Воспользуйтесь конструкцией switch для определения дня недели на основе введенного числа.
  -->
 
 
@@ -48,21 +46,53 @@
 
 
 
-<?php
+ <!DOCTYPE html>
+<html>
+<head>
+    <title>Day of the Week</title>
+</head>
+<body>
+    <h1>Day of the Week</h1>
+    <form method="post" action="">
+        <label for="day">Enter a number from 1 to 7:</label>
+        <input type="number" name="day" id="day" min="1" max="7" required>
+        <br>
+        <input type="submit" value="Check">
+    </form>
 
-$string = "Hello, World!";
-$substring = substr($string, 7, 5);
-echo $substring; // Output: "World"
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $day = $_POST["day"];
 
+        switch ($day) {
+            case 1:
+                echo "Monday";
+                break;
+            case 2:
+                echo "Tuesday";
+                break;
+            case 3:
+                echo "Wednesday";
+                break;
+            case 4:
+                echo "Thursday";
+                break;
+            case 5:
+                echo "Friday";
+                break;
+            case 6:
+                echo "Saturday";
+                break;
+            case 7:
+                echo "Sunday";
+                break;
+            default:
+                echo "Invalid value";
+                break;
+        }
+    }
+    ?>
+</body>
+</html>
 
-$string = "Hello, World!";
-$substring = substr($string, -6, -1);
-echo $substring; // Output: "World"
-
-$string = "Hello, World!";
-$substring = substr($string, 7);
-echo $substring; // Выводит: "World!"
-
-
-?>
 
